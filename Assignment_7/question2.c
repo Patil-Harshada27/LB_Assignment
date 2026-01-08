1058e5a73a296625c9efce86522856f2bb7bf204
Patil-Harshada27 <harshadapatil2704@gmail.com>
2026-01-08 12:05:25 +0530

    Add files via upload

---
+++ Assignment_7/question2.c
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name       : question2.c
+//  Description     : Accept one number from user and print numbers till that number on screen 
+//  Author          : harshada anil patil 
+//  Date            : 22/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+void Display(iNo)
+{ 
+    int iCnt = 0;
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    for (iCnt = 1; iCnt <= iNo; iCnt ++)
+    {
+        printf("%d\t",iCnt);
+    }
+}
+int main()
+{
+   int iValue = 0 ;
+   
+   printf("Enter Your number\n");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+
+  return 0;
+
+
+}
---
+++ Assignment_7/question5.c
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name       : question5.c
+//  Description     : Accept one number from user and print 5 multiples of that number
+//  Author          : harshada anil patil
+//  Date            : 22/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+void  DisplayMultiples(iNo)
+{ 
+
+    int iCnt = 0;
+    int iMult = 1;
+
+    if(iNo < 0)
+    {
+       iNo = -iNo;
+    }
+
+    for (iCnt = 1 ; iCnt <= 5 ; iCnt ++)
+    {
+
+           iMult=iCnt *iNo;
+           printf("%d\t",iMult);
+        
+    }
+}
+int main()
+{
+   int iValue = 0 ;
+   
+   printf("Enter Your number\n");
+   scanf("%d",&iValue);
+
+   DisplayMultiples(iValue);
+
+  return 0;
+
+
+}
