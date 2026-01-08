9779eb2e139b8b9bcf0b8323aad8b0b59d85e714
Patil-Harshada27 <harshadapatil2704@gmail.com>
2026-01-08 12:08:31 +0530

    Add files via upload

---
+++ Assignment_9/question2.c
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name       : question2.c
+//  Description     : Convert USD to INR 
+//  Author          : harshada anil patil 
+//  Date            : 22/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+int USDtoINR(int iNo)
+{
+    int iINR = 0;
+    iINR = 70 * iNo;
+    return iINR;
+}
+
+int main()
+{
+    int iValue = 0 , iRet = 0;
+
+    printf("Enter USD\n");
+    scanf("%d",&iValue);
+
+    iRet = USDtoINR(iValue);
+
+    printf("%d Dollars is %d INR",iValue,iRet);
+
+    return 0;
+}
