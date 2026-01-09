3bc5a3cd65699b4a85d239f958a6ea2c5a87a414
Patil-Harshada27 <harshadapatil2704@gmail.com>
2026-01-09 11:09:54 +0530

    Add files via upload

---
+++ Assignment_13/question2.c
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name       : question2.c
+//  Description     : Accept number from user and print even  number till that number on screen
+//  Author          : harshada anil patil
+//  Date            : 31/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void print_Even_Number(int ilimit)
+
+
+{
+   if (ilimit <= 0)
+   {
+     return;
+   }
+
+   int iCnt = 0 ;
+   
+   for (iCnt = 1; iCnt <= ilimit ; iCnt++)
+   { 
+     
+    if((iCnt % 2 )== 0)
+    {
+      printf("%d\t",iCnt);
+
+    }
+   }
+}
+
+int main()
+{
+
+    int iLimit = 0;
+
+
+
+    printf("Enter your limit\n");
+    scanf("%d",&iLimit);
+
+     print_Even_Number(iLimit);
+    
+    return 0;
